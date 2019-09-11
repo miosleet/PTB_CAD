d09cfb1b2146943f3192e80673c65c3931782dda
MioSleet <43563705+miosleet@users.noreply.github.com>
2019-09-11 13:24:04 +0800

    20190911a
    字符串转数据已经改为输出List<List<double>>，每一行是一个List<double>，每一个元素是这一行的list下的一个元素

--- 工艺战舰船只设计参考/Form1.cs
+++ 工艺战舰船只设计参考/Form1.cs
@@ -37,68 +37,40 @@ namespace 工艺战舰船只设计参考
 		struct Weapon
 		{
 			public int cnt;
-			public int caliber;
+			public double caliber;
 		}//武器
 
-		private List<int> get_input(String data) {//输入的string转int[]
-			List<int> ret_data = new List<int>();
-			if (data.Length == 0) {
-				ret_data.Add(0);
-				return ret_data;
-			}
+		private List<List<double>> get_input(String data) {//输入的string转int[]
+			List<List<double>> ret_data = new List<List<double>>();
+			if (data.Length == 0)return ret_data;
+
 			try
 			{
 				String[] imput_arr = data.Split(new char[2] { '\r', '\n' });
 				for (int i = 0; i < imput_arr.Length; i++)
 				{
 					if (imput_arr[i].Length == 0) continue;
+					List<double> now_data = new List<double>();
 					String[] record = imput_arr[i].Split(new char[1] { ',' });
 					for (int j = 0; j < record.Length; j++)
 					{
-						if (record[j].Length == 0) ret_data.Add(0); ;
-						ret_data.Add(int.Parse(record[j]));
+						if (record[j].Length == 0){
+							now_data.Add(0.0);
+							continue;
+						}
+						now_data.Add(double.Parse(record[j]));
 					}
+					ret_data.Add(now_data);
 				}
 			}
 			catch (FormatException) {
-				ret_data = new List<int>();
-				ret_data.Add(-1);
+				ret_data = new List<List<double>>();
 				MessageBox.Show("输入格式错误");
 			}
 
 			return ret_data;
 		}//输入的string转int[]
-		private List<int> get_Length(String data) {
-			List<int> ret_data = new List<int>();
-			if (data.Length == 0)
-			{
-				ret_data.Add(0);
-				return ret_data;
-			}
-			try
-			{
-				String[] imput_arr = data.Split(new char[2] { '\r', '\n' });
-				for (int i = 0; i < imput_arr.Length; i++)
-				{
-					if (imput_arr[i].Length == 0) continue;
-					String[] record = imput_arr[i].Split(new char[1] { ',' });
-					for (int j = 0; j < record.Length; j++)
-					{
-						if (record[j].Length == 0) continue;
-						ret_data.Add(int.Parse(record[j]));
-					}
-				}
-			}
-			catch (FormatException)
-			{
-				ret_data = new List<int>();
-				ret_data.Add(-1);
-				MessageBox.Show("输入格式错误");
-			}
-
-			return ret_data;
-		}
-
+		
 		private void Form1_Load(object sender, EventArgs e)
 		{
 
@@ -111,11 +83,13 @@ namespace 工艺战舰船只设计参考
 
 		private void textBox1_TextChanged(object sender, EventArgs e)
 		{
+			/*
 			String temp_input = Text_Volume.Text.ToString();
 			if (temp_input.Length == 0) return;
-			List<int> data = get_input(temp_input);
+			List<List<int>> data = get_input(temp_input);
 			if (data.Count > 1) return;
-			dis.Volume = 0.2 * data[0];
+			dis.Volume = 0.2 * data[0][0];
+			*/
 		}
 
 		private void label5_Click(object sender, EventArgs e)
@@ -127,8 +101,9 @@ namespace 工艺战舰船只设计参考
 		{
 			int[] hp = { 0, 58496, 100000, 132192, 158496, 180735, 200000, 216992, 232192, 245943, 258496, 270043, 280735, 290689, 300000, 308746, 316992, 324792, 332192, 339231, 345943 };
 			String temp_input = text_Smk.Text.ToString();
-			List<int> data = get_input(temp_input);
-			int smk_cnt = data[0];
+			if (temp_input.Length == 0) return;
+			List<List<double>> data = get_input(temp_input);
+			int smk_cnt = (int)data[0][0];
 			if (smk_cnt > 20) smk_cnt = 20;
 			label_Power.Text = hp[smk_cnt].ToString();
 		}//烟数量 输入
@@ -140,10 +115,10 @@ namespace 工艺战舰船只设计参考
 
 
 
-		private double calc_mgz_cnt(Weapon []data) {
+		private double calc_mgz_cnt(List<Weapon> data) {
 			//一根管的弹药消耗：M=0.0001*C^2
 			double cnt=0.0;
-			for (int i = 0; i < data.Length; i++) {
+			for (int i = 0; i < data.Count; i++) {
 				if (data[i].caliber >= 500) cnt += data[i].cnt;
 				else cnt += 0.0001 * data[i].cnt * Math.Pow(data[i].caliber, 2);
 			}
@@ -154,12 +129,13 @@ namespace 工艺战舰船只设计参考
 		{
 			string temp_input = text_Weapon.Text.ToString();
 			if (temp_input.Length > 0) {
-				List<int> data = get_input(temp_input);
-				if (data.Count == 1 && data[0] == -1||data.Count%2==1) return;
-				Weapon[] data_arr = new Weapon[data.Count / 2];
-				for (int i = 0; i < data.Count/2; i++) {
-					data_arr[i].cnt = data[2 * i];
-					data_arr[i].caliber = data[2 * i + 1];
+				List<List<double>> data = get_input(temp_input);
+				List<Weapon> data_arr = new List<Weapon>();
+				for (int i = 0; i < data.Count; i++) {
+					if (data[i].Count % 2 == 1) return;
+					Weapon temp = new Weapon();
+					temp.cnt = (int)data[i][0];temp.caliber = data[i][1];
+					data_arr.Add(temp);
 				}
 				double mgz_cnt = calc_mgz_cnt(data_arr);
 				label_Mgz.Text = mgz_cnt.ToString();
